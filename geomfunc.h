f6a4186fff2586769bd31e314b21e10d05fd04b7
cicada <cicada@kali.8.8.8.8>
2022-02-26 23:46:33 -0500

    Final formatting geomfunc.h

--- geomfunc.h
+++ geomfunc.h
@@ -2,32 +2,40 @@
 #define STRINGSIZE 30
 #define COORDSIZE 5
 
-int isOpnBracket(char *arr, int c) {
+int isOpnBracket(char* arr, int c)
+{
     for (int c = 0; c < STRINGSIZE; c++)
-        if (arr[c] == '(') return c;
+        if (arr[c] == '(')
+            return c;
     return 0;
 }
 
-int isClsBracket(char *arr, int cnt) {
+int isClsBracket(char* arr, int cnt)
+{
     for (int x = cnt; x < STRINGSIZE; x++)
-        if (arr[x] == ')') return x;
+        if (arr[x] == ')')
+            return x;
     return 0;
 }
 
-int isCorrectName(char *nameObject, char *cir, int cirSize) {
+int isCorrectName(char* nameObject, char* cir, int cirSize)
+{
     for (int x = 0; x < cirSize; x++) {
-        if (nameObject[x] != cir[x] && toupper(nameObject[x]) != cir[x] &&
-            tolower(nameObject[x]) != cir[x]) {
+        if (nameObject[x] != cir[x] && toupper(nameObject[x]) != cir[x]
+            && tolower(nameObject[x]) != cir[x]) {
             return 0;
         }
     }
     return 1;
 }
 
-int isCorrectCntComma(char *arrCoords) {
+int isCorrectCntComma(char* arrCoords)
+{
     unsigned short commaCnt;
     for (int x = 0; x < STRINGSIZE; x++)
-        if (arrCoords[x] == ',') commaCnt++;
-    if (commaCnt == 1) return 1;
+        if (arrCoords[x] == ',')
+            commaCnt++;
+    if (commaCnt == 1)
+        return 1;
     return 0;
 }
